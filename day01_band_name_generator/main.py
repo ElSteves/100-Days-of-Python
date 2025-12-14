283a70b7ed87de38073c0a84f19ac732f32ea05c
ElSteves <stevencadena12@gmail.com>
2025-12-14 11:43:01 -0500

    Implement basic band name generator logic
    En el dia 1 aprendi a imprimir, dar nombre a variables y usar estas variables luego

---
+++ day01_band_name_generator/main.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name generator app")
+city = input("What city you grew up in?\n")
+pet = input("Whats your pet´s name?\n")
+print("Your band´s name could be " + city + " " + pet)

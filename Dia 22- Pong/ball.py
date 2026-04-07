69f51a0476a734fd3a33417eb3dd1d03381e3cf5
ElSteves <stevencadena12@gmail.com>
2026-04-07 13:53:40 -0500

    Add Ball class with movement and collision logic
    Implement Ball class for Pong game with movement and collision detection.

---
+++ Dia 22- Pong/ball.py
@@ -0,0 +1,32 @@
+from turtle import Turtle
+import random as rd
+
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color("white")
+        self.shape("circle")
+        self.penup()
+        self.x_move = 10
+        self.y_move = 10
+
+    def bounce(self):
+        self.y_move *= -1
+
+    def move(self):
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
+        self.goto(new_x, new_y)
+
+    def detect_wall(self):
+        if self.ycor() >= 280 or self.ycor() <= -280:
+            self.bounce()
+
+    def detect_paddle(self, object):
+        if self.distance(object) < 50 and (self.xcor() < 320 or self.xcor() > -320):
+            self.y_move *= -1
+            self.x_move *= -1
+
+    def reset_position(self):
+        self.goto(0, 0)
+        self.x_move *= -1
